ee2ee8ebaa60bd500e69ade8881b73b432d5743d
Michael Sy <michaeldominiquesy@gmail.com>
2025-10-18 14:11:26 -0400

    Add dj-database-url to requirements.txt and change default database

--- recycling/settings.py
+++ recycling/settings.py
@@ -91,15 +91,11 @@ WSGI_APPLICATION = 'recycling.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': dj_database_url.config(
-    #     conn_max_age=600,
-    #     conn_health_checks=True,
-    #     ssl_require=True
-    # ),
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        conn_health_checks=True,
+        ssl_require=True
+    ),
     'local': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
