d2c1c0d51df7c58091f13fedb9a0cc43c9a3196a
Michael Sy <michaeldominiquesy@gmail.com>
2025-11-06 13:14:53 -0500

    Change environ reading

--- project_b_04/settings.py
+++ project_b_04/settings.py
@@ -22,8 +22,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env(DEBUG=(bool,False))
 
-if os.path.exists(BASE_DIR / '.env'):
-    environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
